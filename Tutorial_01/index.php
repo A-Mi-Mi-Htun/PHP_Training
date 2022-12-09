c4db6ad7bc2b7fb239227d4e2587a9abfc633897
amimihtun <leonwhit22@gmail.com>
2022-12-09 13:59:30 +0630

    fixed review

--- Tutorial_01/index.php
+++ Tutorial_01/index.php
@@ -1,42 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tutorial 01</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <table class="table">
-  <?php
-    echo "<h1>Chessboard</h1>";
-    $col = 0;
+<?php
+echo "<h1>Chessboard</h1>";
+$col = 0;
 
-    while($col < 8)
-    {
-      $row = 0;
-      echo "<tr>";
-      $value = $col;
+while ($col < 8) {
+    $row = 0;
+    echo "<tr>";
+    $value = $col;
 
-      while($row < 8)
-      {
-        if($value%2 == 0)
-        {
-          echo "<td class='white-td'></td>";
-          $value++;
-        }else
-        {
-          echo "<td class='black-td'></td>";
-          $value++;
+    while ($row < 8) {
+        if (($value % 2) == 0) {
+            echo "<td class='white-td'></td>";
+            $value++;
+        } else {
+            echo "<td class='black-td'></td>";
+            $value++;
         }
         $row++;
-      }
-      echo "</tr>";
-      $col++;
     }
-  ?>
-  </table>
-  
-</body>
-</html>
+    echo "</tr>";
+    $col++;
+}
+?>
