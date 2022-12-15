2a219da4a40766d1df92ac6d1f94c6d5ca7f7471
amimihtun <leonwhit22@gmail.com>
2022-12-16 01:49:18 +0630

    fixed review

---
+++ Tutorial_05/README.md
@@ -0,0 +1,5 @@
+# Tutorial_05
+
+<b>Preview</b>
+
+<img src="preview.png">
--- Tutorial_05/index.php
+++ Tutorial_05/index.php
@@ -1,73 +1,72 @@
-<?php 
-/**
- *Read and display txt fie 
- */
-echo "<h4>Text File</h4>";
-$txtFile = file_get_contents("file/sample.txt");
-echo "<pre>$txtFile</pre>";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    
-<body>
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/style.css">
+<div class="container">
+    <h3 class="main-ttl">Tutorial_05</h3>
     <?php
+    error_reporting(0);
+
     /**
-     *Read and display csv file 
+     *Read and display txt fie
      */
-    echo "<table>";
-    echo "<h4>CSV File</h4>";
+    echo "<div class='box-one'>";
+    echo "<h4 class='ttl'>Text File</h4>";
+    $txtFile = file_get_contents("file/sample.txt");
+    echo "<pre class='txt'>$txtFile</pre>";
+    echo "</div>";
+    
+    /**
+     *Read and display csv file
+     */
+    echo "<div class='box-two'>";
+    echo "<table class='csv-table'>";
+    echo "<h4 class='ttl'>CSV File</h4>";
     $csvFile = fopen("file/sample.csv", "r");
-    while(($data = fgetcsv($csvFile)) !== false) {
+    while (($data = fgetcsv($csvFile)) !== false) {
         echo "<tr>";
         foreach ($data as $i) {
             echo "<td>" . htmlspecialchars($i) . "</td>";
         }
-        echo "</tr><br>";
+        echo "</tr>";
     }
     fclose($csvFile);
     echo "</table>";
-    ?>
-</body>
-</html>
-
-<?php
-/**
- * 
- * Read and display docx file 
- */
-echo "<h4>Document File</h4>";
-include("lib/doc2txt.class.php");
-$docObj = new Doc2Txt("file/sample.docx");
-$documentFile = $docObj->convertToText();
-echo "<pre>$documentFile</pre";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <table>
-        <?php
-        /**
-         * Read and display excel file
-         */
-        echo "<h4>Excel File</h4>";
-        require("lib/vendor/autoload.php");
-        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-        $spreadsheet = $reader->load("file/sample.xlsx");
-        $worksheet = $spreadsheet->getActiveSheet();
-
-        foreach ($worksheet->getRowIterator() as $row) {
-            $cellIterator = $row->getCellIterator();
-            $cellIterator->setIterateOnlyExistingCells(false);
-
-            echo "<tr>";
-            foreach ($cellIterator as $cell) {
-                echo "<td>" . $cell->getValue() . "</td>";
-            }
-            echo "</tr>";
+    echo "</div>";
+    
+    /**
+     *
+     * Read and display docx file
+     */
+    echo "<div class='box-three'>";
+    echo "<h4 class='ttl'>Document File</h4>";
+    include "lib/doc2txt.class.php";
+    $docObj = new Doc2Txt("file/sample.docx");
+    $documentFile = $docObj->convertToText();
+    echo "<pre class='txt'>$documentFile</pre>";
+    echo "</div>";
+    
+    /**
+     * Read and display excel file
+     */
+    echo "<div class='box-four'>";
+    echo "<table class='excel-table'>";
+    echo "<h4 class='ttl'>Excel File</h4>";
+    require "lib/vendor/autoload.php";
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+    $spreadsheet = $reader->load("file/sample.xlsx");
+    $worksheet = $spreadsheet->getActiveSheet();
+    
+    foreach ($worksheet->getRowIterator() as $row) {
+        $cellIterator = $row->getCellIterator();
+        $cellIterator->setIterateOnlyExistingCells(false);
+    
+        echo "<tr>";
+        foreach ($cellIterator as $cell) {
+            echo "<td>" . $cell->getValue() . "</td>";
         }
-        ?>
-    </table>
-</body>
-</html>
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "</div>";
+    ?>
+    
+</div>
